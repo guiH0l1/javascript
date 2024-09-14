6ea78043f297e4cc1b760ae58ce92b3305680538
Guilherme Holi <guilhermeholi09@gmail.com>
2024-09-13 21:32:38 -0300

    Variaveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,21 @@
+/**Estudo de constantes de variaveis
+ * @author Guilherme Holi
+ * */
+
+console.clear()
+
+let nome 
+let idade
+let  vip
+
+nome = "Guilherme Oliveira"
+console.log(typeof(nome))
+console.log(nome)
+
+idade = 21
+console.log(typeof(idade))
+console.log(idade)
+
+vip = true
+console.log(typeof(vip))
+console.log(vip)
