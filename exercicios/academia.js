eecceab2c502d6de4636baed1c939c3c825641b6
Guilherme Holi <guilhermeholi09@gmail.com>
2024-10-15 22:17:33 -0300

    adicionar e limpar a função

--- exercicios/academia.js
+++ exercicios/academia.js
@@ -11,7 +11,7 @@ console.clear()
 
 // entrada de dados
 nome = "Guilherme Holi"
-peso = 90
+peso = 120
 idade = 21
 altura = 1.55
 vip = true
@@ -32,16 +32,16 @@ console.log(`Vip ${vip}`)
 console.log(`FCM ${fcm}`)
 console.log(`IMC ${imc.toFixed(2)}`)
 
-if (imc <= 25 && imc >= 15){
-    console.log("Peso normal")
-} else if (imc > 25 && imc <= 30) {
-    console.log(`Acima do peso`)
-}else if (imc > 30 && imc <= 35) {
+if (imc < 18.5){
+    console.log("Abaixo do peso")
+} else if (imc < 25) {
+    console.log(`Peso normal`)
+}else if (imc < 30) {
+    console.log(`Levemente acima do peso`)
+} else if (imc < 35) {
     console.log(`Obesidade grau 1`)
-} else if (imc > 35 && imc <= 40) {
+} else if (imc < 40) {
     console.log(`Obesidade grau 2`)
-} else if (imc > 40) {
-    console.log(`Obesidade grau 3`)
 } else {
-    console.log(`Abaixo do peso`)
+    console.log(`Obesidade grau 3`)
 }
