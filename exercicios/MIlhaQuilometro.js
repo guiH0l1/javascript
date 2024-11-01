62afdddaa86b0eccd925146dfeded58f5d5c27c4
Guilherme Holi <guilhermeholi09@gmail.com>
2024-10-31 22:12:17 -0300

    Milha e quilometro

--- exercicios/MIlhaQuilometro.js
+++ exercicios/MIlhaQuilometro.js
@@ -3,3 +3,16 @@
 Desenvolva um programa que converta uma distância em milhas para quilômetros.
 @author Guilherme Holi
  */
+
+
+console.clear()
+console.log("CONVERTER MILHA PARA KILOMETRO")
+const input = require('readline-sync')
+
+let milhas, km
+
+milhas = Number(input.question("Insira um valor para converter milhas para quilometros: "))
+
+km = milhas * 1.6
+
+console.log(`O valor de ${milhas} milhas convertido é ${km.toFixed(2)}km`)
